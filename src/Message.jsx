0f90b976c38dd9b910cc12bd3d5a3c022e7a3db7
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-02-14 03:37:32 +0530

    add class based on the logged in user

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,12 +1,16 @@
 import { Avatar } from "@material-ui/core";
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
 import "./Message.css";
 function Message({
   id,
   contents: { displayName, photo, email, message, timestamp },
 }) {
+  const user = useSelector(selectUser);
+
   return (
-    <div className="message">
+    <div className={`message ${user.email === email && "message__sender"}`}>
       <Avatar src={photo} />
       <p>{message}</p>
       <small>{new Date(timestamp?.toDate()).toLocaleString()}</small>
