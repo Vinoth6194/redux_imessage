8ad701fbf6f25d22805c1879ce8edb446926e902
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-02-13 23:22:33 +0530

    sidebarChat__info

--- src/SidebarChat.jsx
+++ src/SidebarChat.jsx
@@ -5,7 +5,7 @@ function SidebarChat() {
   return (
     <div className="sidebarChat">
       <Avatar />
-      <div className="sidebarChats__info">
+      <div className="sidebarChat__info">
         <h3>Channel Name</h3>
         <p>last message sent </p>
         <small>timestamp</small>
