28dc1de9989b94292a5d0871500697d55bcbfbb4
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-02-14 01:15:53 +0530

    login css

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import "./App.css";
 import Imessage from "./Imessage";
 import { selectUser } from "./features/userSlice";
 import { useSelector } from "react-redux";
+import Login from "./Login";
 
 function App() {
   const user = useSelector(selectUser);
-  return <div className="app">{user ? <Imessage /> : "You need to Login"}</div>;
+  return <div className="app">{user ? <Imessage /> : <Login />}</div>;
 }
 
 export default App;
