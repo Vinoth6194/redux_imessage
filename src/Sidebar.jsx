828ca8f717954ad62c6875464be1745bd61ca1cf
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-02-14 01:53:03 +0530

    chats are fetched from the database to the sidebar on load

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Avatar, IconButton } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 import RateReviewIcon from "@material-ui/icons/RateReview";
@@ -6,9 +6,21 @@ import "./Sidebar.css";
 import SidebarChat from "./SidebarChat";
 import { selectUser } from "./features/userSlice";
 import { useSelector } from "react-redux";
-import { auth } from "./firebase";
+import db, { auth } from "./firebase";
 function Sidebar() {
   const user = useSelector(selectUser);
+  const [chats, setChats] = useState([]);
+
+  useEffect(() => {
+    db.collection("chats").onSnapshot((snapshot) =>
+      setChats(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      )
+    );
+  }, []);
   return (
     <div className="sidebar">
       <div className="sidebar__header">
