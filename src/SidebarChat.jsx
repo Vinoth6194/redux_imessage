123a831eb82c2bfb9c34e916fe5838cb34e712a5
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-02-14 04:00:04 +0530

    fetched the chat info from the firebase in the sidebarhgats

--- src/SidebarChat.jsx
+++ src/SidebarChat.jsx
@@ -1,10 +1,23 @@
 import { Avatar } from "@material-ui/core";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { setChat } from "./features/chatSlice";
+import db from "./firebase";
 import "./SidebarChat.css";
 function SidebarChat({ id, chatName }) {
   const dispatch = useDispatch();
+  const [chatInfo, setChatInfo] = useState([]);
+  useEffect(() => {
+    console.log("id", id);
+
+    db.collection("chats")
+      .doc(id)
+      .collection("messages")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        setChatInfo(snapshot.docs.map((doc) => doc.data()))
+      );
+  }, [id]);
   return (
     <div
       className="sidebarChat"
@@ -20,6 +33,8 @@ function SidebarChat({ id, chatName }) {
       <Avatar />
       <div className="sidebarChat__info">
         <h3>{chatName}</h3>
+        {console.log("Chat info", chatInfo)}
+
         <p>last message sent </p>
         <small>timestamp</small>
       </div>
