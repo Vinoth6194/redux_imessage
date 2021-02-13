72f4fac4ca5eeb897db10199f989f379156faf01
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-02-14 00:56:57 +0530

    firebasee configured

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+const firebaseConfig = {
+  apiKey: "AIzaSyDCUAypDrhBH7YBT3-h-VBWq4TWOmxRD40",
+  authDomain: "redux-imessage.firebaseapp.com",
+  projectId: "redux-imessage",
+  storageBucket: "redux-imessage.appspot.com",
+  messagingSenderId: "451804994198",
+  appId: "1:451804994198:web:700a2df79517026fa500b7",
+  measurementId: "G-FNWQ00SFDK",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebaseConfig.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
