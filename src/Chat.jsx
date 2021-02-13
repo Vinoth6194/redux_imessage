4b9ad6e36bdcdce4a56f706dd9c86b6adb1e25ac
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-02-13 23:56:59 +0530

    simple chat compose

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Chat.css";
 function Chat() {
+  const [input, setInput] = useState("");
+  const sendMessage = (e) => {
+    e.preventDefault();
+    setInput("");
+  };
   return (
     <div className="chat">
       {/* chat header */}
@@ -12,6 +17,17 @@ function Chat() {
       </div>
       {/* chat messages */}
       {/* chat input */}
+      <div className="chat__input">
+        <form>
+          <input
+            type="text"
+            placeholder="iMessage"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          ></input>
+          <button onClick={sendMessage}>Send Message</button>
+        </form>
+      </div>
     </div>
   );
 }
