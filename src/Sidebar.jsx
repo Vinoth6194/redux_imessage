dd54889636cdaff9ac3e6e0abe25b939adebae7c
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-02-13 23:26:41 +0530

    sidebarChats scroll

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -19,6 +19,15 @@ function Sidebar() {
       </div>
       <div className="sidebar__chats">
         <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
       </div>
     </div>
   );
