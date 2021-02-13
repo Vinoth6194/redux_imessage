8f2f21649d8bc5e2732d9aad21ccb5848536d6c0
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-02-13 23:37:57 +0530

    chat template disp

---
+++ src/Chat.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Chat.css";
+function Chat() {
+  return (
+    <div className="chat">
+      {/* chat header */}
+      <h3>To:Channel name</h3>
+      {/* chat messages */}
+      {/* chat input */}
+    </div>
+  );
+}
+
+export default Chat;
